6471ef7f2e8b8c12ade10a95b9b18664f4b71498
zz <zhengzh@microsoft.com>
2020-06-12 18:11:13 -0700

    Added test device successfully

--- wechat/src/services/deviceService.ts
+++ wechat/src/services/deviceService.ts
@@ -4,35 +4,25 @@ import { Mqtt as IoTHubMqtt } from 'azure-iot-device-mqtt';
 import { ProvisioningDeviceClient } from 'azure-iot-provisioning-device';
 import { SymmetricKeySecurityClient } from 'azure-iot-security-symmetric-key';
 import { Mqtt as DPSMqtt } from 'azure-iot-provisioning-device-mqtt';
+import { RegistrationClient } from 'azure-iot-provisioning-device/lib/interfaces';
 
 const host = 'global.azure-devices-provisioning.net';
+const deviceId = 'rqusekqzej17q44qfk9a4y';
 const scopeId = '0ne00117F1D';
-const key = 'KA4Vfez1m+P1nnfaZWck53JDLHj14tBT/ibsmh9kySfNCSXLttp2hRMmP6Y4WFSilBT1WFEDsnB/83tK0wl8oA==';
+const key = 'nh0KrCGJm36MrhALpVF+ZwC20d9vppWWJ2NhPb2QSlU=';
 
 export async function createDevice(req: express.Request, res: express.Response) {
     try {
-        const deviceId = Date.now().toString();
+        // const deviceId = req.query.id as string || Date.now().toString();
         const dpsTransport = new DPSMqtt();
         const securityClient = new SymmetricKeySecurityClient(deviceId, key);
         const dpsClient = ProvisioningDeviceClient.create(host, scopeId, dpsTransport, securityClient);
 
-        dpsClient.register((err, result) => {
-            if (err) {
-                callback(err, null);
-            } else {
-                const connectionString = 'HostName=' + result.assignedHub + ';DeviceId=' + result.deviceId + ';SharedAccessKey=' + key;
-                const iothubClient = Client.fromConnectionString(connectionString, IoTHubMqtt);
-
-                iothubClient.open(err => {
-                    if (err) {
-                        callback(err, null);
-                    } else {
-                        callback(null, iothubClient);
-                    }
-                });
-            }
+        const iothubClient = await registerDevice(dpsClient);
+        iothubClient.emit(deviceId, 'values');
+        res.status(200).json({
+            deviceId
         });
-        res.status(200).json();
     } catch (e) {
         const error = {
             code: 400,
@@ -42,7 +32,27 @@ export async function createDevice(req: express.Request, res: express.Response) 
     }
 }
 
-function callback(error: any, iothubClient: any) {
-    console.log(error);
-    console.log(iothubClient);
+async function registerDevice(dpsClient: RegistrationClient): Promise<Client> {
+    return new Promise((resolve, reject) => {
+        dpsClient.register((err, result) => {
+            if (err) {
+                console.log('11111111');
+                reject(err);
+            } else {
+                const connectionString = `HostName=${result.assignedHub};DeviceId=${result.deviceId};SharedAccessKey=${key}`;
+                console.log(connectionString);
+                const iothubClient = Client.fromConnectionString(connectionString, IoTHubMqtt);
+                console.log(iothubClient);
+
+                iothubClient.open(err => {
+                    if (err) {
+                        console.log('2222222');
+                        reject(err);
+                    } else {
+                        resolve(iothubClient);
+                    }
+                });
+            }
+        });
+    });
 }
